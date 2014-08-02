64b14373fbc51fc8a2600b90ce1f654464d16907
Nathan Armstrong <kenogu.labz@gmail.com>
2014-08-02 11:26:36 -0700

    Remove behavior from empty player interaction script.

--- Freefall.Unity/Assets/EmptyPlayerInteraction.cs
+++ Freefall.Unity/Assets/EmptyPlayerInteraction.cs
@@ -2,14 +2,8 @@
 using System.Collections;
 
 public class EmptyPlayerInteraction : PlayerInteraction {
-	private int i = 0;
 
 	override protected InteractionState PerformInteraction (){
-		if(++i == 200){
-			i = 0;
-			return InteractionState.Pending;
-		}
-
-		return InteractionState.Running;
+		return InteractionState.Pending;
 	}
 }
