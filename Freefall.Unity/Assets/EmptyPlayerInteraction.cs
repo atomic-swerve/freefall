a9b6939d91788535337df5c7111c6441557f3c38
Nathan Armstrong <kenogu.labz@gmail.com>
2014-08-02 11:20:49 -0700

    Add a script for defining the behaviour of player-interactable objects, as well as a default 'empty' implementation.

---
+++ Freefall.Unity/Assets/EmptyPlayerInteraction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmptyPlayerInteraction : PlayerInteraction {
+	private int i = 0;
+
+	override protected InteractionState PerformInteraction (){
+		if(++i == 200){
+			i = 0;
+			return InteractionState.Pending;
+		}
+
+		return InteractionState.Running;
+	}
+}
---
+++ Freefall.Unity/Assets/PlayerInteraction.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PlayerInteraction : MonoBehaviour
+{
+		protected enum InteractionState
+		{
+				Pending,
+				Running,
+				Complete
+		}
+	
+		/*
+		 * Override this with the behaviour to perform.
+	 	 * Use 'yield' to finish behaviour processing for the current frame.
+	 	 *
+	 	 * - Returning 'Pending' will stop interaction processing and
+	 	 *   allow the player to restart the interaction.
+	 	 * - Returning 'Running' will continue running the interaction
+	 	 *   processing on the subsequent frame.
+	 	 * - Returning 'Complete' will permanently disable the interaction.
+	 	 */
+		abstract protected InteractionState PerformInteraction ();
+	
+		// Internal mechanisms
+		void Start ()
+		{
+				IsPlayerColliding = false;
+				CurrentInteractionState = InteractionState.Pending;
+				SpriteRenderer = this.GetComponent<SpriteRenderer> ();
+		}
+
+		void Update ()
+		{
+				if (IsPlayerColliding && IsAttemptingInteraction && IsStatePending) {
+						CurrentInteractionState = InteractionState.Running;
+				}
+				if (IsStateRunning) {
+						CurrentInteractionState = PerformInteraction ();
+				}
+				if (IsStateComplete) {
+						Destroy (this);
+				}
+
+				SpriteRenderer.enabled = IsStatePending;
+		}
+
+		void OnCollisionEnter2D (Collision2D collision)
+		{
+				if (collision.gameObject.tag == "Player") {
+						IsPlayerColliding = true;
+				}
+		}
+
+		void OnCollisionExit2D (Collision2D collision)
+		{
+				if (collision.gameObject.tag == "Player") {
+						IsPlayerColliding = false;
+				}
+		}
+
+		private bool IsAttemptingInteraction {
+				get {
+						return Input.GetButtonDown ("B");
+				}
+		}
+	
+		private bool IsPlayerColliding { get; set; }
+
+		private InteractionState CurrentInteractionState { get; set; }
+	
+		private bool IsStatePending {
+			get {
+				return CurrentInteractionState == InteractionState.Pending;
+			}
+		}
+		
+		private bool IsStateRunning {
+			get {
+				return CurrentInteractionState == InteractionState.Running;
+			}
+		}
+		
+		private bool IsStateComplete {
+			get {
+				return CurrentInteractionState == InteractionState.Complete;
+			}
+		}
+	
+		private SpriteRenderer SpriteRenderer { get; set; }
+}
