c0d9849c308f3fbd9533cfc126943f087245e1c5
Tony Lee <lee.anthony0490@gmail.com>
2014-08-02 16:15:46 -0700

    Pausing the game

---
+++ Freefall.Unity/Assets/Scripts/PauseController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseController : MonoBehaviour {
+    public bool isPaused = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown("space")) 
+        {
+            isPaused = !isPaused;
+            pauseGame(isPaused);
+        }
+	}
+
+    // Pause or unpause the game depending on the pause state
+    void pauseGame(bool isPaused)
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            //print("Game is paused.");
+        }
+        else
+        {
+            Time.timeScale = 1;
+            //print("Game is unpaused");
+        }
+    }
+}
