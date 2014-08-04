622c77d0e071ca5e51f838b953236b98e3cdcf0e
Tony Lee <lee.anthony0490@gmail.com>
2014-08-03 22:31:12 -0700

    Testing for player attack

--- Freefall.Unity/Assets/Scripts/PlayerAttackInteraction.cs
+++ Freefall.Unity/Assets/Scripts/PlayerAttackInteraction.cs
@@ -3,74 +3,19 @@ using System.Collections;
 
 public class PlayerAttackInteraction : PlayerInteraction {
 
-    PlayerController player;
-
-    Vector2 UP_DIRECTION = new Vector2(0, 1);
-    Vector2 DOWN_DIRECTION = new Vector2(0, -1);
-    Vector2 RIGHT_DIRECTION = new Vector2(1, 0);
-    Vector2 LEFT_DIRECTION = new Vector2(-1, 0);
-
 	// Use this for initialization
 	void Start () {
-	    player = this.GetComponent<PlayerController>();
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        PerformAttack();
+	
 	}
 
-    override protected InteractionState PerformInteraction()
+    protected override InteractionState PerformInteraction()
     {
-        PerformAttack();
+        Destroy(gameObject);
         return InteractionState.Complete;
-    }
-
-    void PerformAttack()
-    {
-        Vector2 attackBox = new Vector2(1,1);
-        
-        float attackDistance = 1.0f;
-
-        if (Input.GetButtonDown("Attack") && Input.GetAxis("Y-Axis") > 0)
-        {
-            Attack(player.transform.position, attackBox, UP_DIRECTION, attackDistance);
-        }
-        if (Input.GetButtonDown("Attack") && Input.GetAxis("X-Axis") < 0)
-        {
-            Attack(player.transform.position, attackBox, LEFT_DIRECTION, attackDistance);
-        }
-        if (Input.GetButtonDown("Attack") && Input.GetAxis("X-Axis") > 0)
-        {
-            Attack(player.transform.position, attackBox, RIGHT_DIRECTION, attackDistance);
-        }
-        if (Input.GetButtonDown("Attack") && Input.GetAxis("Y-Axis") < 0)
-        {
-            if (player.jumping || player.gliding)
-                Attack(player.transform.position, attackBox, DOWN_DIRECTION, attackDistance);
-        }
-        if (Input.GetButtonDown("Attack"))
-        {
-            Attack(player.transform.position, attackBox, player.facingVector, attackDistance);
-        }
-    }
-
-    void Attack(Vector2 position, Vector2 attackBoxSize, Vector2 attackDirection, float attackDistance)
-    {
-        RaycastHit2D[] hitObjects = Physics2D.BoxCastAll(position, attackBoxSize, 0, attackDirection, attackDistance);
-        if (hitObjects.Length == 0)
-            print("Nothing hit");
-        else
-        {
-            // Put attack logic in here
-            string text = "Items Hit";
-            for (int i = 0; i < hitObjects.Length; i++)
-            {
-                text += "(" + hitObjects[i].transform.position + "), ";
-                Destroy(hitObjects[i].collider.gameObject);
-            }
-            print(text);
-        }
-        print(attackDirection.ToString());
     }
 }
