994027660480c47315eb4e303a4159111610ef67
Andrew Hung <druphoria@utexas.edu>
2014-08-07 02:20:41 -0700

    got rise through platforms working for real

--- Freefall.Unity/Assets/Scripts/GroundChecker.cs
+++ Freefall.Unity/Assets/Scripts/GroundChecker.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 public class GroundChecker : MonoBehaviour {
 	// Detection of a ground tile within this distance of bottom left/corner/right of collider will cause 
 	// checkGrounded() to evaluate to true.
-	public float colliderToGroundDistance = 2f;
+	public float colliderToGroundDistance = .2f;
 
 	private BoxCollider2D boxCollider2D;
+	private CircleCollider2D circleCollider2D;
 
 	void Awake() {
 		boxCollider2D = GetComponent<BoxCollider2D>();
+		circleCollider2D = GetComponent<CircleCollider2D>();
 	}
 
 	public bool CheckGrounded(int groundLayerIndex) {
@@ -65,7 +67,8 @@ public class GroundChecker : MonoBehaviour {
 	}
 
 	public void HandleRiseThroughPlatforms() {
-		int groundLayerIndex = LayerMask.NameToLayer("RiseThrough Ground");
+		int riseThroughGroundLayerIndex = LayerMask.NameToLayer("RiseThrough Ground");
+
 		float halfColliderWidth = (boxCollider2D.size.x * transform.localScale.x) / 2;
 		float halfColliderHeight = (boxCollider2D.size.y * transform.localScale.y) / 2;
 
@@ -74,21 +77,46 @@ public class GroundChecker : MonoBehaviour {
 
 		Vector2 boxCenter = new Vector2(boxCenter3D.x, boxCenter3D.y);
 		
-		Vector2 boxBottomCenter = new Vector2(boxCenter.x, boxCenter.y - halfColliderHeight - 8);
-		Vector2 boxLowerLeftCorner = new Vector2(boxCenter.x - halfColliderWidth, boxCenter.y - halfColliderHeight - 8);
-		Vector2 boxLowerRightCorner = new Vector2(boxCenter.x + halfColliderWidth, boxCenter.y - halfColliderHeight - 8);
+		// Subtract raisedPlatformTileSize so that the raycast doesn't un-ignore the collision layer while player is still passing through the tile.
+		Vector2 bottomCenter = new Vector2(boxCenter.x, boxCenter.y - halfColliderHeight);
+		Vector2 lowerLeft = new Vector2(boxCenter.x - halfColliderWidth, boxCenter.y - halfColliderHeight);
+		Vector2 lowerRight = new Vector2(boxCenter.x + halfColliderWidth, boxCenter.y - halfColliderHeight);
+
+		Vector2 topCenter = new Vector2(boxCenter.x, boxCenter.y + halfColliderHeight);
+		Vector2 topLeft = new Vector2(boxCenter.x - halfColliderWidth, boxCenter.y + halfColliderHeight);
+		Vector2 topRight = new Vector2(boxCenter.x + halfColliderWidth, boxCenter.y + halfColliderHeight);
 
 		Vector2 downwardsVector = new Vector2(0, -1);
 		Vector2 upwardsVector = new Vector2(0, 1);
 
-		// Draw a ray down the left edge, center, and right edge in order to detect ground.
-		if (Physics2D.Raycast(boxBottomCenter, downwardsVector, Mathf.Infinity, 1 << groundLayerIndex)
-			|| Physics2D.Raycast(boxLowerLeftCorner, downwardsVector, Mathf.Infinity, 1 << groundLayerIndex)
-			|| Physics2D.Raycast(boxLowerRightCorner, downwardsVector, Mathf.Infinity, 1 << groundLayerIndex)
-			&& !Physics2D.Raycast(boxBottomCenter, upwardsVector, .2f, 1 << groundLayerIndex)) {
-				Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Default"), LayerMask.NameToLayer("RiseThrough Ground"), false);
-		} else {
-			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Default"), LayerMask.NameToLayer("RiseThrough Ground"), true);
+		RaycastHit2D middleDownHit = Physics2D.Raycast(bottomCenter, downwardsVector, Mathf.Infinity, 1 << riseThroughGroundLayerIndex);
+		RaycastHit2D leftDownHit = Physics2D.Raycast(lowerLeft, downwardsVector, Mathf.Infinity, 1 << riseThroughGroundLayerIndex);
+		RaycastHit2D rightDownHit = Physics2D.Raycast(lowerRight, downwardsVector, Mathf.Infinity, 1 << riseThroughGroundLayerIndex);
+
+		RaycastHit2D middleUpHit = Physics2D.Raycast(topCenter, upwardsVector, Mathf.Infinity, 1 << riseThroughGroundLayerIndex);
+		RaycastHit2D leftUpHit = Physics2D.Raycast(topLeft, upwardsVector, Mathf.Infinity, 1 << riseThroughGroundLayerIndex);
+		RaycastHit2D rightUpHit = Physics2D.Raycast(topRight, upwardsVector, Mathf.Infinity, 1 << riseThroughGroundLayerIndex);
+
+		List<RaycastHit2D> downHits = new List<RaycastHit2D>();
+		downHits.Add(middleDownHit);
+		downHits.Add(leftDownHit);
+		downHits.Add(rightDownHit);
+
+		List<RaycastHit2D> upHits = new List<RaycastHit2D>();
+		upHits.Add(middleUpHit);
+		upHits.Add(leftUpHit);
+		upHits.Add(rightUpHit);
+
+		if(!Physics2D.Raycast(bottomCenter, upwardsVector, colliderToGroundDistance, 1 << riseThroughGroundLayerIndex)) {
+			foreach(RaycastHit2D hit in downHits) {
+				Physics2D.IgnoreCollision(boxCollider2D, hit.collider, false);
+				Physics2D.IgnoreCollision(circleCollider2D, hit.collider, false);
+			}
+		}
+
+		foreach(RaycastHit2D hit in upHits) {
+			Physics2D.IgnoreCollision(boxCollider2D, hit.collider, true);
+			Physics2D.IgnoreCollision(circleCollider2D, hit.collider, true);
 		}
 	}
 }
