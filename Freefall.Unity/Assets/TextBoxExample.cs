a4b19a17208478a7ea30f3d99821873006e65ad2
Jarbuckle <jdarbuckle@gmail.com>
2014-08-09 10:38:50 -0700

    Created an example scene using the new text box system.

---
+++ Freefall.Unity/Assets/TextBoxExample.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextBoxExample : MonoBehaviour {
+
+	void Update () {
+		TextBoxUI textBox = GUIManager.GetCurrentGUI().TextBox;
+		if ((!textBox.gameObject.activeInHierarchy) && (Input.GetButtonDown ("Start"))) {
+			textBox.Show("example.txt");
+		}
+	}
+}
