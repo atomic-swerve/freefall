2d8cf6795c26eef75391e870c9d2d00d0a91f2f9
Tony Lee <lee.anthony0490@gmail.com>
2014-08-08 23:14:05 -0700

    Initial version of audio wrapper

---
+++ Freefall.Unity/Assets/Scripts/AudioController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void PlaySound(AudioSource audio)
+    {
+        audio.Play();
+    }
+
+    public void StopSound(AudioSource audio)
+    {
+        if (audio.isPlaying)
+            audio.Stop();
+    }
+
+    public void PauseSound(AudioSource audio)
+    {
+        audio.Pause();
+    }
+
+    public void setSoundLoop(AudioSource audio)
+    {
+        audio.loop = true;
+    }
+
+    public void PlayDelayedSound(AudioSource audio, float delaySeconds)
+    {
+        audio.PlayDelayed(delaySeconds);
+    }
+}
