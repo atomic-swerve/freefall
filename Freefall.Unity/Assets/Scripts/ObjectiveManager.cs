dac362cd7887deb5226dde74216affbdd328f470
Nathan Armstrong <kenogu.labz@gmail.com>
2014-08-09 19:12:10 -0700

    Add an objective manager for tracking objective sequences and registering objective completions.

---
+++ Freefall.Unity/Assets/Scripts/ObjectiveManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectiveManager : MonoBehaviour {
+	public IList<IObjective> Objectives { get; set; }
+
+	private IObjective CurrentObjective {
+		get {
+			return Objectives.Count == 0 ? null : Objectives[0];
+		}
+	}
+
+	public bool ReceiveObjectiveTrigger<T>() where T:IObjective {
+		if(CurrentObjective == null){
+			Debug.LogWarning(
+				"Received objective trigger attempt when no objective remains to be completed.  " +
+				"Objective type received: " + typeof(T).FullName, this);
+			return false;
+		}
+
+		if (CurrentObjective is T) {
+			CompleteCurrentObjective();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private void CompleteCurrentObjective(){
+		IObjective completedObjective = CurrentObjective;
+		Objectives.RemoveAt(0);
+		completedObjective.Complete();
+	}
+}
