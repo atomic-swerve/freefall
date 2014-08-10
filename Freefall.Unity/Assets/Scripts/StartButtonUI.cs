dd6796a4e15fd5d8b6b25fe20ff8079e9c79cbad
Jarbuckle <jdarbuckle@gmail.com>
2014-08-10 14:56:56 -0700

    Full implementation of the Main Menu. Also moved some scripts into the Scripts folder.

---
+++ Freefall.Unity/Assets/Scripts/StartButtonUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButtonUI : MenuButtonUI {
+
+	public string SceneName = "Scenes/Playgrounds/LevelGenTest";
+
+	public override void OnActivated() {
+		Application.LoadLevel(SceneName);
+	}
+}
