98ceb2bb184a7f21a9844fe5b4b1e796fe758dd0
Jarbuckle <jdarbuckle@gmail.com>
2014-08-09 21:39:07 -0700

    Created following and interpolating behaviors for the main camera.

---
+++ Freefall.Unity/Assets/CameraMotion.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMotion : MonoBehaviour {
+
+    public Transform TargetToFollow;
+    public bool FollowingTarget = true;
+    
+    public struct CameraMotionInterp {
+		public Vector2 initialPosition;
+        public Vector2 targetPosition;
+        public float duration;
+    }
+    
+    private CameraMotionInterp currentInterp;
+	private bool interpolating = false;
+	private float interpStartTime = 0.0f;
+
+	public void BeginInterpolation(CameraMotionInterp interp, bool continueFromCurrent = false) {
+		if (continueFromCurrent) {
+			interp = new CameraMotionInterp() { 
+				initialPosition = transform.position,
+				targetPosition = interp.targetPosition,
+				duration = interp.duration
+			};
+		}
+		currentInterp = interp;
+		FollowingTarget = false;
+		interpolating = true;
+		interpStartTime = Time.time;
+	}
+
+	public void StopInterpolation() {
+		interpolating = false;
+		interpStartTime = 0.0f;
+	}
+
+	public bool IsInterpolating() {
+		return interpolating;
+	}
+
+	public void FollowTarget(Transform target = null) {
+		if (target != null) {
+			TargetToFollow = target;
+		}
+		if (TargetToFollow != null) {
+			FollowingTarget = true;
+		}
+		StopInterpolation();
+	}
+
+	void Update () {
+		if ((TargetToFollow != null) && FollowingTarget) {
+			Vector3 targetPos = TargetToFollow.transform.position;
+			transform.position = new Vector3(targetPos.x, targetPos.y, transform.position.z);
+		} else if (interpolating) {
+			Vector2 pos;
+			float currentTime = Time.time;
+			float timeDelta = currentTime - interpStartTime;
+			float timeRatio = timeDelta / currentInterp.duration;
+			if (timeRatio < 1.0f) {
+				pos = Vector2.Lerp(currentInterp.initialPosition, 
+				                   currentInterp.targetPosition, 
+				                   timeRatio);
+			} else {
+				pos = currentInterp.targetPosition;
+				StopInterpolation();
+			}
+			transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+        }
+	}
+}
