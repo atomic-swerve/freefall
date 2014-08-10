89c9cd546effe45cf13ed3769641b6d8b9548b86
atomic-swerve <lppsmanabeast@gmail.com>
2014-08-10 15:25:47 -0700

    Added actual start game scene

--- Freefall.Unity/Assets/Scripts/StartButtonUI.cs
+++ Freefall.Unity/Assets/Scripts/StartButtonUI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class StartButtonUI : MenuButtonUI {
 
-	public string SceneName = "Scenes/Playgrounds/LevelGenTest";
+	public string SceneName = "Scenes/Levels/level1";
 
 	public override void OnActivated() {
 		Application.LoadLevel(SceneName);
