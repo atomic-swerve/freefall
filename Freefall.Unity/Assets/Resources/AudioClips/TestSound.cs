3562fd845a0ddd2e1f7a881f004369b3bb987c66
Tony Lee <lee.anthony0490@gmail.com>
2014-08-10 03:14:23 -0700

    New version of audio api

---
+++ Freefall.Unity/Assets/Resources/AudioClips/TestSound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioAPI audioAPI = this.GetComponent<AudioAPI>();
+        audioAPI.PlayTheme("freefall_home_island");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Freefall.Unity/Assets/Scripts/AudioAPI.cs
+++ Freefall.Unity/Assets/Scripts/AudioAPI.cs
@@ -1,40 +1,94 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AudioController : MonoBehaviour {
+public class AudioAPI : MonoBehaviour {
+
+    Hashtable audioHashtable = new Hashtable();
+    AudioSource audioIntro;
+    AudioSource audioLoop;
 
 	// Use this for initialization
 	void Start () {
-
+        //PlayTheme("freefall_home_island");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        
 	}
 
-    public void PlaySound(AudioSource audio)
+    public void PlayTheme(string audioName)
     {
-        audio.Play();
+        string audioIntroName = audioName + "_intro";
+        string audioLoopName = audioName + "_loop";
+
+        audioIntro = gameObject.AddComponent<AudioSource>();
+        audioLoop = gameObject.AddComponent<AudioSource>();
+
+        float introSecs = PlayIntroSong(audioIntroName);
+        PlayLoopSong(audioLoopName, introSecs);
     }
 
-    public void StopSound(AudioSource audio)
+    public void StopSound()
     {
-        if (audio.isPlaying)
-            audio.Stop();
+        if (audioLoop.isPlaying)
+            audioLoop.Stop();
     }
 
     public void PauseSound(AudioSource audio)
     {
-        audio.Pause();
+        if (audioLoop.isPlaying)
+            audioLoop.Pause();
     }
 
-    public void setSoundLoop(AudioSource audio)
+    public void ResumeSound(AudioSource audio)
     {
-        audio.loop = true;
+        if (!audioLoop.isPlaying && audioLoop.clip != null)
+            audioLoop.Play();
     }
 
-    public void PlayDelayedSound(AudioSource audio, float delaySeconds)
+    public void PlaySFX(string audioName)
     {
-        audio.PlayDelayed(delaySeconds);
+        AudioClip audioClip;
+
+        if (!audioHashtable.ContainsKey(audioName))
+            LoadAudio(audioName);;
+
+        audioClip = (AudioClip)audioHashtable[audioName];
+        audioLoop.PlayOneShot(audioClip);
+    }
+
+    private void LoadAudio(string audioName)
+    {
+        AudioClip audioClip = (AudioClip)Resources.Load("AudioClips/" + audioName);
+        audioHashtable.Add(audioName, audioClip);
+    }
+
+    private float PlayIntroSong(string audioName)
+    {
+        AudioClip audioClip;
+
+        if (!audioHashtable.ContainsKey(audioName))
+            LoadAudio(audioName);
+
+        audioClip = (AudioClip)audioHashtable[audioName];
+        audioIntro.clip = audioClip;
+        audioIntro.Play();
+
+        return audioClip.length;
+    }
+
+    private void PlayLoopSong(string audioName, float delaySecs)
+    {
+        AudioClip audioClip;
+
+        if (!audioHashtable.ContainsKey(audioName))
+            LoadAudio(audioName);
+
+        audioLoop.loop = true;
+
+        audioClip = (AudioClip)audioHashtable[audioName];
+        audioLoop.clip = audioClip;
+        audioLoop.PlayDelayed(delaySecs);
     }
 }
