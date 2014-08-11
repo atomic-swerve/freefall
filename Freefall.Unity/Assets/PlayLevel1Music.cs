3b9a3826b197ed545dc7619e57b652ca9224eadb
atomic-swerve <lppsmanabeast@gmail.com>
2014-08-10 21:04:41 -0700

    Added music to level 1

---
+++ Freefall.Unity/Assets/PlayLevel1Music.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayLevel1Music : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioAPI.PlayTheme ("freefall_home_island");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
