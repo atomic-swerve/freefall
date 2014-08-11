6b2a48775c785a83a2e9c34e95c3b7a700b43d75
atomic-swerve <lppsmanabeast@gmail.com>
2014-08-10 20:44:31 -0700

    Added text, added menu music

---
+++ Freefall.Unity/Assets/MainMenuMusic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioAPI.PlaySong ("freefall_title", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
