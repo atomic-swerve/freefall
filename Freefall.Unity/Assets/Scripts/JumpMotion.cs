aa309dc1d626f04700a630d263e9888cf2265cea
Jarbuckle <jdarbuckle@gmail.com>
2014-08-10 20:25:30 -0700

    Character animation, yo!

--- Freefall.Unity/Assets/Scripts/JumpMotion.cs
+++ Freefall.Unity/Assets/Scripts/JumpMotion.cs
@@ -21,12 +21,18 @@ public class JumpMotion : MonoBehaviour {
 	public void InitiateJump() {
 		Vector2 newVelocity = new Vector2(rigidbody2D.velocity.x, jumpVelocity);
 		rigidbody2D.velocity = newVelocity;
+		Animator animator = GetComponent<Animator>();
+		animator.SetBool("Jumping", true);
 	}
 
 	public void HandleJumpState() {
+		Animator animator = GetComponent<Animator>();
 		if(rigidbody2D.velocity.y <= 0) {
 			player.Jumping = false;
-		}	
+			animator.SetBool("Jumping", false);
+		} else if ((rigidbody2D.velocity.y > -float.Epsilon) && (rigidbody2D.velocity.y < float.Epsilon)) {
+
+		}
 
 		if(!Input.GetButton("A") && player.Jumping) {
 			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y - jumpDecelerationRate);				
