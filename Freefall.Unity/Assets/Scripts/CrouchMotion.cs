6276bc1cfd16a7c9e0704d7a9c7520b77eac594c
Andrew Hung <druphoria@utexas.edu>
2014-08-11 20:14:50 -0700

    fixed collider placement for alex and changed ground movement values to be less fucked up

--- Freefall.Unity/Assets/Scripts/CrouchMotion.cs
+++ Freefall.Unity/Assets/Scripts/CrouchMotion.cs
@@ -14,8 +14,8 @@ public class CrouchMotion : MonoBehaviour {
 	public void ActivateCrouch() {
 		if(!player.Crouching) {
 			player.Crouching = true;
-			box.center = new Vector2 (0, 11);
-			box.size = new Vector2 (10, 22);
+			//box.center = new Vector2 (0, 11);
+			//box.size = new Vector2 (10, 22);
 			// Do all other crouch-related logic here
 		}
 	}
@@ -23,8 +23,8 @@ public class CrouchMotion : MonoBehaviour {
 	public void DeactivateCrouch() {
 		if(player.Crouching) {
 			player.Crouching = false;
-			box.center = new Vector2 (0, 14);
-			box.size = new Vector2 (10, 28);
+			//box.center = new Vector2 (0, 14);
+			//box.size = new Vector2 (10, 28);
 			// Do all other crouch-related logic here
 		}
 	}
