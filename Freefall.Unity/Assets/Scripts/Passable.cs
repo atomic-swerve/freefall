858ce4d76d56a110db262d77fe85aae12872fd79
Andrew Hung <druphoria@utexas.edu>
2014-08-16 10:51:00 -0700

    added method for upper diagonal hits

--- Freefall.Unity/Assets/Scripts/Passable.cs
+++ Freefall.Unity/Assets/Scripts/Passable.cs
@@ -18,6 +18,9 @@ public class Passable : MonoBehaviour {
 
 	private int numVerticalRays = 4;
 	private int numHorizontalRays = 6;
+	private int numDiagonalRays = 4;
+
+	private Rect box;
 
 	void Awake() {
 		player = GetComponent<PlayerController>();
@@ -29,17 +32,18 @@ public class Passable : MonoBehaviour {
 	}
 
 	public void HandlePassablePlatforms() {
+		box = new Rect(boxCollider2D.bounds.min.x, boxCollider2D.bounds.min.y, boxCollider2D.bounds.size.x, boxCollider2D.bounds.size.y);
+
 		HandlePassableTilesBelow();
 		HandleLateralHits();
 		HandlePassableTilesAbove();
+		HandleDiagonalHits();
 	}
 
 	/**
 	* Handles all passable tiles below the player. All passable tiles below the player will be set to collide with player.
 	*/
 	private void HandlePassableTilesBelow() {
-		Rect box = new Rect(boxCollider2D.bounds.min.x, boxCollider2D.bounds.min.y, boxCollider2D.bounds.size.x, boxCollider2D.bounds.size.y);
-
 		List<RaycastHit2D> downHits = 
 			GetRaycastResults(new Vector2(box.xMin, box.yMin), new Vector2(box.xMax, box.yMin), -Vector2.up, Mathf.Infinity, numVerticalRays);
 		List<RaycastHit2D> upHits = 
@@ -47,9 +51,7 @@ public class Passable : MonoBehaviour {
 
 		// Activate collisions for tiles found below player's bottom edge only if player's bottom edge is not currently passing through a passable tile.
 		if(upHits.Count == 0 && !player.DroppingThroughPlatform) {
-			foreach(RaycastHit2D hit in downHits) {
-				Physics2D.IgnoreCollision(boxCollider2D, hit.collider, false);
-			}
+			SetIgnoreCollisionForHits(downHits, false);
 		}
 	}
 
@@ -57,37 +59,43 @@ public class Passable : MonoBehaviour {
 	* Handles all passable tiles above the player. All passable tiles above the player will be set to ignore collision.
 	*/
 	private void HandlePassableTilesAbove() {
-		Rect box = new Rect(boxCollider2D.bounds.min.x, boxCollider2D.bounds.min.y, boxCollider2D.bounds.size.x, boxCollider2D.bounds.size.y);
-		
 		List<RaycastHit2D> upHits = 
 			GetRaycastResults(new Vector2(box.xMin, box.yMax), new Vector2(box.xMax, box.yMax), Vector2.up, Mathf.Infinity, numVerticalRays);
 
-		// Disable collisions for tiles found above player so that player can jump through them.
-		foreach(RaycastHit2D hit in upHits) {
-			Physics2D.IgnoreCollision(boxCollider2D, hit.collider, true);
-		}
+		SetIgnoreCollisionForHits(upHits, true);
 	}
 
 	/**
 	* Handles all passable tiles to left/right of the player. All passable tiles to sides of the player will be set to ignore collision.
 	*/
 	private void HandleLateralHits() {
-		Rect box = new Rect(boxCollider2D.bounds.min.x, boxCollider2D.bounds.min.y, boxCollider2D.bounds.size.x, boxCollider2D.bounds.size.y);
-
 		List<RaycastHit2D> leftLateralHits = 
 			GetRaycastResults(new Vector2(box.xMin, box.yMax), new Vector2(box.xMin, box.yMin), -Vector2.right, Mathf.Infinity, numHorizontalRays);
 		List<RaycastHit2D> rightLateralHits = 
 			GetRaycastResults(new Vector2(box.xMax, box.yMax), new Vector2(box.xMax, box.yMin), Vector2.right, Mathf.Infinity, numHorizontalRays);
 
-		foreach(RaycastHit2D hit in leftLateralHits) {
-			Physics2D.IgnoreCollision(boxCollider2D, hit.collider, true);
-		}
-
-		foreach(RaycastHit2D hit in rightLateralHits) {
-			Physics2D.IgnoreCollision(boxCollider2D, hit.collider, true);
-		}
+		SetIgnoreCollisionForHits(leftLateralHits, true);
+		SetIgnoreCollisionForHits(rightLateralHits, true);
 	}
 
+	private void HandleDiagonalHits() {
+		List<RaycastHit2D> upperRightHits =
+			GetRaycastResults(new Vector2(box.center.x, box.yMax), new Vector2(box.xMax, box.center.y), new Vector2(1, 1), Mathf.Infinity, numDiagonalRays);
+
+		List<RaycastHit2D> upperLeftHits = 
+			GetRaycastResults(new Vector2(box.center.x, box.yMax), new Vector2(box.xMin, box.center.y), new Vector2(-1, 1), Mathf.Infinity, numDiagonalRays);
+
+		List<RaycastHit2D> upperDiagonalHits = new List<RaycastHit2D>(upperRightHits);
+		upperDiagonalHits.AddRange(upperLeftHits);
+
+		SetIgnoreCollisionForHits(upperDiagonalHits, true);
+	}
+
+	private void SetIgnoreCollisionForHits(List<RaycastHit2D> hits, bool ignoreCollision) {
+		foreach(RaycastHit2D hit in hits) {
+			Physics2D.IgnoreCollision(boxCollider2D, hit.collider, ignoreCollision);
+		}
+	}
 
 	private List<RaycastHit2D> GetRaycastResults(Vector2 startPoint, Vector2 endPoint, Vector2 direction, float distance, int numRays) {
 		List<RaycastHit2D> result = new List<RaycastHit2D>();
@@ -97,7 +105,6 @@ public class Passable : MonoBehaviour {
 			Vector2 origin = Vector2.Lerp(startPoint, endPoint, lerpAmount);
 
 			RaycastHit2D[] hits = Physics2D.RaycastAll(origin, direction, distance, 1 << passableGroundLayerIndex);
-			Debug.DrawRay(origin, direction * 20, Color.green);
 
 			result.AddRange(hits);
 		}
