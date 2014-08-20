c88bfe2538d68e24ded0aed4bee02d38e0494080
Andrew Hung <druphoria@utexas.edu>
2014-08-19 21:35:40 -0700

    added wolf prefab to level

---
+++ Freefall.Unity/Assets/Scripts/Constants/RaycastLayers.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastLayers {
+
+	public static readonly int groundLayer;
+
+	static RaycastLayers() {
+		groundLayer = 1 << LayerMask.NameToLayer("Ground") | 1 << LayerMask.NameToLayer("Passable");
+	}
+}
